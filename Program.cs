8b39f8109b1450750fe044686c5ca4ebc4cefce0
shockz-pulseHome <shockzinfinity@gmail.com>
2021-01-12 00:31:05 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ServiceReference1.ListServiceClient client = new ServiceReference1.ListServiceClient();
+      ServiceReference1.CancelRequest[] requests = new ServiceReference1.CancelRequest[]
+      {
+        new ServiceReference1.CancelRequest()
+      };
+      requests[0].RequestId = "temp";
+      requests[0].RequestServiceKey = "temp service key";
+
+      var resutls = client.GetResponses(requests);
+            
+      Console.ReadLine();
+    }
+  }
+}
